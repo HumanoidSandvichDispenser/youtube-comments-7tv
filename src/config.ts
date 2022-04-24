060031c05e12954a98407ace554508eb3c607a6e
HumanoidSandvichDispenser <25856867+HumanoidSandvichDispenser@users.noreply.github.com>
2022-04-23 18:55:20 -0700

    configuration file

---
+++ src/config.ts
@@ -0,0 +1,22 @@
+/*
+ * config.ts
+ * Copyright (C) 2022 sandvich <sandvich@artix>
+ *
+ * Distributed under terms of the GPLv3 license.
+ */
+
+
+const config = {
+    /*
+     * Time, in seconds, for the emote cache to live. If the emote cache is
+     * older than the specified value, then the script will fetch channel
+     * emotes again, as long as the previous channel visited was the same.
+     */
+    cacheTTL: 3600,
+
+    /**
+     * Enable global emotes in comments sections of any channel, not just those
+     * linked to 7TV.
+     */
+    globalEmotesEverywhere: false,
+}
